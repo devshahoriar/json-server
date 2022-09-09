bcd2727e6a7166289ab991a6f930e007418aaa8f
Shahoriar <shahoriar.dev@gmail.com>
2022-09-09 19:35:27 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const videos = require("./data/videos");
 const tags = require("./data/tags");
 const transactions = require("./data/db_transactions");
+const todos = require("./data/db_todos");
 
 
 module.exports = {
+  todos,
   videos,
   tags,
   transactions,
